404c4f5aba30d34b5f46a4b1fa09eeb37bf606fe
CAN <cvtcn-ayglr4712@hotmail.com>
2023-10-09 18:36:18 +0300

    9 Ekim

--- index.py
+++ index.py
@@ -1,16 +1,51 @@
 import requests as req
 from bs4 import BeautifulSoup as bs
+import time
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-url="https://boxofficeturkiye.com/hafta/detay/2023-32"
+sender = "denemeyapcaz321@hotmail.com"
+password = "deneme321"
+
+receiver = "cevat.can.ayguler@hotmail.com"
+subject = "Yeni Duyuru"
+#---
+
+url="https://erzurum.edu.tr/duyuru/index/1052/1/#gsc.tab=0"
 r=req.get(url)
 soup= bs(r.content,"lxml")
-
-filmtablosu= soup.find("table",attrs={"id":"WeeklyMovieData"}).select("tr")
-# soup.find("table",attrs={"class":"ustcizgi"}).select("tr:nth-of-type(2)>td>table:nth-of-type(3) > tr")
-# class değeri ustcizgi olan table altındaki 2.trnin altındaki td nni altındaki 3.table ın altındaki tr
-for i in range(1,21):
-    filmadi=filmtablosu[i].find("a",attrs={"class":"movie-link"}).get("title")
-    toplamhasilat=filmtablosu[i].select("td:nth-of-type(8) > span")[0].text
-    toplamseyirci=filmtablosu[i].select("td:nth-of-type(9)")[0].text
-    print("Film Adi:{} \nHasilat: {} \nToplam Seyirci: {}".format(filmadi,toplamhasilat,toplamseyirci))
-    print("-"*30)
+tablo= soup.find("div",attrs={"class":"list"}).select("div")
+yazdir=""
+a=0
+tarih=""
+while True:
+    if(tarih!=tablo[0].text):
+        yazdir+="---\tDUYURULAR\t---\n"
+        for i in range(0,4):
+            if (i%2==0):
+                yazdir+=f"Tarih: {tablo[i].text}\n"
+            else:
+                line=tablo[i].text
+                line=line.lstrip()
+                line=line.rstrip()
+                yazdir+=f"Konu: {line}\n{"-"*30}\n"
+        tarih=tablo[0].text
+        message = MIMEMultipart()
+        message['From'] = sender
+        message['To'] = receiver
+        message['Subject'] = subject
+        message.attach(MIMEText(yazdir, 'plain'))
+        try:
+            server = smtplib.SMTP("smtp.office365.com", 587)
+            server.starttls()
+            server.login(sender, password)
+            print("Giriş başarılı")
+            server.sendmail(sender, receiver, message.as_string())
+            print("Gönderim tamamlandı")
+            server.quit()
+        except Exception as e:
+            print("An error occurred:", str(e))
+    else:
+        pass
+    time.sleep(10)
